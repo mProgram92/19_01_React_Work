5d25da2d53d691fd38b239a644463daa017ab574
Web Developer <98003931+mProgram92@users.noreply.github.com>
2022-01-20 09:58:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Form from './componets/Form';
+import Table from './componets/Table';
+
+
+function App(){
+    return <div className='container'>
+        <Table></Table>
+        <Form></Form>
+    </div>
+}
+
+export default App;
